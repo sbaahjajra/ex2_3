85e9884d4e1230ada9900f2a3fd683038baa9a5c
User1 <User1@10.0.0.1>
2024-02-04 13:26:09 +0200

    version-3

--- Root-classificatin/src/main/java/Root_classificatin/Root_classificatin/Roots.java
+++ Root-classificatin/src/main/java/Root_classificatin/Root_classificatin/Roots.java
@@ -28,7 +28,7 @@ class Roots
       
       q = b*b - 4*a*c;
      
-      if (out_of_Range(a, 0, 50)|| out_of_Range(b, 0, 50)||out_of_Range(c, 0, 100)) {
+      if (out_of_Range(a, 0, 100)|| out_of_Range(b, 0, 100)||out_of_Range(c, 0, 100)) {
     	  roots = Root_Types.ERROR;
     	  return;
       }
@@ -36,26 +36,26 @@ class Roots
       if (a != 0) {
 	      if(q>0 )
 	      {
-		      roots = Root_Types.Equal_roots; 
+		      roots = Root_Types.Two_roots; 
 	      }
 	      else if(q==0)
 	      {
 		      // the equation has exactly one root
 		     
 		     
-		      roots = Root_Types.Two_roots; 
+		      roots = Root_Types.Equal_roots; 
 		      
 	      } 
 	      else
 	      {
 		      // the equation has no roots if b^2 < 4ac
 		     
-		      roots = Root_Types.Not_quadratic;
+		      roots = Root_Types.No_roots;
 	      }
       }
       else {
     	  
-    	  roots = Root_Types.Two_roots; 
+    	  roots = Root_Types.Not_quadratic; 
       }
    }
 
--- Root-classificatin/src/test/java/Root_classificatin/Root_classificatin/RootsTest.java
+++ Root-classificatin/src/test/java/Root_classificatin/Root_classificatin/RootsTest.java
@@ -8,15 +8,213 @@ public class RootsTest
 	Roots root;
 
 	@Test
-	public void testPartion1() 
+	public void testPartion1_1() 
 	{
-		//(0,1,2)
-		Roots.calculate_roots(-2, 1, 2);
+		//(-2,5,4)
+		Roots.calculate_roots(-2, 5, 4);
 		
 		String  root_expected = Root_Types.ERROR.toString();
 		
 		
 		assertEquals(root_expected, Roots.root_types().toString());
 	}
+
+	@Test
+	public void testPartion_a_min() 
+	{
+		//(0,5,4)
+		Roots.calculate_roots(0, 5, 4);
+		
+		String  root_expected = Root_Types.Not_quadratic.toString();
+		
+		
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+
+	
+	@Test
+	public void testPartion_a_minplos() 
+	{
+		//(1,5,2)
+		Roots.calculate_roots(1, 5, 2);
+		
+		String  root_expected = Root_Types.Two_roots.toString();
+		
+		
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+	
+	
+	@Test
+	public void testPartion_a_nominal() 
+	{
+		//(3,10,2)
+		Roots.calculate_roots(3, 10, 2);
+		
+		String  root_expected = Root_Types.Two_roots.toString();
+		
+		
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+	
+	
+	@Test
+	public void testPartion_a_max() 
+	{
+		//(100,50,4)
+		Roots.calculate_roots(100, 50, 4);
+		
+		String  root_expected = Root_Types.Two_roots.toString();
+		
+		
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+	
+	
+	@Test
+	public void testPartion_a_maxminos() 
+	{
+		//(99,80,5)
+		Roots.calculate_roots(99, 80, 5);
+		
+		String  root_expected = Root_Types.Two_roots.toString();
+		
+		
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+	
+	
+	@Test
+	public void testPartion_b_min() 
+	{
+		//(5,0,4)
+		Roots.calculate_roots(5, 0, 4);
+		
+		String  root_expected = Root_Types.No_roots.toString();
+		
+		
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+
+	
+	@Test
+	public void testPartion_b_minplos() 
+	{
+		//(3,1,3)
+		Roots.calculate_roots(3, 1, 3);
+		
+		String  root_expected = Root_Types.No_roots.toString();
+		
+		
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+	
+	
+	@Test
+	public void testPartion_b_nominal() 
+	{
+		//(4,4,4)
+		Roots.calculate_roots(4, 4, 4);
+		
+		String  root_expected = Root_Types.No_roots.toString();
+		
+		
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+	
+	
+	@Test
+	public void testPartion_b_max() 
+	{
+		//(20,100,50)
+		Roots.calculate_roots(20, 100, 50);
+		
+		String  root_expected = Root_Types.Two_roots.toString();
+		
+		
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+	
+	
+	@Test
+	public void testPartion_b_maxminos() 
+	{
+		//(30,99,4)
+		Roots.calculate_roots(30, 99, 4);
+		
+		String  root_expected = Root_Types.Two_roots.toString();
+		
+		
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+	
+	
+	@Test
+	public void testPartion_c_min() 
+	{
+		//(3,4,0)
+		Roots.calculate_roots(3, 4, 0);
+		
+		String  root_expected = Root_Types.Two_roots.toString();
+		
+		
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+
+	
+	@Test
+	public void testPartion_c_minplos() 
+	{
+		//(4,4,1)
+		Roots.calculate_roots(4, 4, 1);
+		
+		String  root_expected = Root_Types.Equal_roots.toString();
+		
+		
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+	
+	
+	@Test
+	public void testPartion_c_nominal() 
+	{
+		//(10,10,10)
+		Roots.calculate_roots(10, 10, 10);
+		
+		String  root_expected = Root_Types.No_roots.toString();
+		
+		
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+	
+	
+	@Test
+	public void testPartion_c_max() 
+	{
+		//(40,40,100)
+		Roots.calculate_roots(40, 40, 100);
+		
+		String  root_expected = Root_Types.No_roots.toString();
+		
+		
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+	
+	
+	@Test
+	public void testPartion_c_maxminos() 
+	{
+		//(3,5,99)
+		Roots.calculate_roots(3, 5, 99);
+		
+		String  root_expected = Root_Types.No_roots.toString();
+		
+		
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+
+
+	
+
 	
 	}
